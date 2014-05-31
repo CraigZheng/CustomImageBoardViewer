1a21968454540b37254bfcf92df95063e7932730
CraigZheng <craignineten@gmail.com>
2014-05-31 22:24:52 +1000

    adding notification system

---
+++ CustomImageBoardViewer/czzNotification.h
@@ -0,0 +1,14 @@
+//
+//  czzNotification.h
+//  CustomImageBoardViewer
+//
+//  Created by Craig Zheng on 31/05/2014.
+//  Copyright (c) 2014 Craig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface czzNotification : NSObject
+
+-(id)initWithXMLData:(NSData*)xmlData;
+@end
---
+++ CustomImageBoardViewer/czzNotification.m
@@ -0,0 +1,13 @@
+//
+//  czzNotification.m
+//  CustomImageBoardViewer
+//
+//  Created by Craig Zheng on 31/05/2014.
+//  Copyright (c) 2014 Craig. All rights reserved.
+//
+
+#import "czzNotification.h"
+
+@implementation czzNotification
+
+@end
