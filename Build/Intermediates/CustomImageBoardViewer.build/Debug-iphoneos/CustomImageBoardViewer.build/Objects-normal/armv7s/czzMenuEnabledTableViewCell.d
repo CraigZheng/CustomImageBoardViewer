1064f06283c36f22999713a551c5bd7b1dd3b0e8
CraigZheng <craignineten@gmail.com>
2014-06-03 10:14:10 +1000

    regular commit

--- Build/Intermediates/CustomImageBoardViewer.build/Debug-iphoneos/CustomImageBoardViewer.build/Objects-normal/armv7s/czzMenuEnabledTableViewCell.d
+++ Build/Intermediates/CustomImageBoardViewer.build/Debug-iphoneos/CustomImageBoardViewer.build/Objects-normal/armv7s/czzMenuEnabledTableViewCell.d
@@ -7,4 +7,11 @@ dependencies: \
   /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/czzPostViewController.h \
   /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/czzBlacklistEntity.h \
   /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/czzAppDelegate.h \
+  /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/czzHomeViewController.h \
+  /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/czzForumsViewController.h \
+  /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/GADBannerView.h \
+  /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/GADAdSize.h \
+  /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/GADBannerViewDelegate.h \
+  /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/GADRequest.h \
+  /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/GADRequestError.h \
   /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/DACircularProgress/DACircularProgressView.h
--- Build/Intermediates/CustomImageBoardViewer.build/Debug-iphoneos/CustomImageBoardViewer.build/Objects-normal/armv7s/czzMoreInfoViewController.d
+++ Build/Intermediates/CustomImageBoardViewer.build/Debug-iphoneos/CustomImageBoardViewer.build/Objects-normal/armv7s/czzMoreInfoViewController.d
@@ -10,4 +10,6 @@ dependencies: \
   /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/czzXMLDownloader.h \
   /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/SMXMLDocument.h \
   /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/czzAppDelegate.h \
+  /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/czzHomeViewController.h \
+  /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/czzForumsViewController.h \
   /Users/craig/Developer/CustomImageBoardViewer/CustomImageBoardViewer/Toast/Toast+UIView.h
