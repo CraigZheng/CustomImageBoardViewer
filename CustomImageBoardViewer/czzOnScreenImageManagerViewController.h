29ca487acfb4020a73faa70c4674acf899c5d2e6
Craig Zheng <Craig@Haozhengs-MacBook-Pro.local>
2014-11-18 12:39:10 +1100

    added on screen image manager

---
+++ CustomImageBoardViewer/czzOnScreenImageManagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  czzOnScreenImageManagerViewController.h
+//  CustomImageBoardViewer
+//
+//  Created by Craig Zheng on 18/11/2014.
+//  Copyright (c) 2014 Craig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface czzOnScreenImageManagerViewController : UIViewController
+
+@end
