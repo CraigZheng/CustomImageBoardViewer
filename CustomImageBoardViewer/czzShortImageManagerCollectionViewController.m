52183e622c8282cb902794c92628e5e51648a758
Haozheng Zheng <haozheng.zheng@optus.com.au>
2014-11-28 14:09:14 +1100

    regular commit

--- CustomImageBoardViewer/czzShortImageManagerCollectionViewController.m
+++ CustomImageBoardViewer/czzShortImageManagerCollectionViewController.m
@@ -10,11 +10,14 @@
 #import "UIView+MGBadgeView.h"
 
 @interface czzShortImageManagerCollectionViewController ()
-
+@property czzImageCentre *imageCentre;
+@property NSArray *downloaders;
 @end
 
 @implementation czzShortImageManagerCollectionViewController
 @synthesize delegate;
+@synthesize imageCentre;
+@synthesize downloaders;
 
 static NSString * const reuseIdentifier = @"Cell";
 static NSString *imageCellIdentifier = @"image_cell_identifier";
@@ -22,31 +25,24 @@ static NSString *imageCellIdentifier = @"image_cell_identifier";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    imageCentre = [czzImageCentre sharedInstance];
     
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = NO;
     
-    // Register cell classes
-    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
-    
-    // Do any additional setup after loading the view.
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    downloaders = imageCentre.currentImageDownloaders.allObjects;
 }
 
 #pragma mark <UICollectionViewDataSource>
 
-- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-#warning Incomplete method implementation -- Return the number of sections
-    return 0;
-}
-
-
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-#warning Incomplete method implementation -- Return the number of items in the section
-    return 0;
+    return downloaders.count;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:imageCellIdentifier forIndexPath:indexPath];
     
     // Configure the cell
     
