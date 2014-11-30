fd8e29f2e308e5e71c99e1138f5dc8c7e9215b01
Craig Zheng <Craig@Haozhengs-MBP.gateway>
2014-11-30 22:37:21 +1100

    added sub thread list

---
+++ CustomImageBoardViewer/czzSubThreadList.h
@@ -0,0 +1,13 @@
+//
+//  czzSubThreadList.h
+//  CustomImageBoardViewer
+//
+//  Created by Craig Zheng on 30/11/2014.
+//  Copyright (c) 2014 Craig. All rights reserved.
+//
+
+#import "czzThreadList.h"
+
+@interface czzSubThreadList : czzThreadList
+
+@end
---
+++ CustomImageBoardViewer/czzSubThreadList.m
@@ -0,0 +1,13 @@
+//
+//  czzSubThreadList.m
+//  CustomImageBoardViewer
+//
+//  Created by Craig Zheng on 30/11/2014.
+//  Copyright (c) 2014 Craig. All rights reserved.
+//
+
+#import "czzSubThreadList.h"
+
+@implementation czzSubThreadList
+
+@end
