d217bd3ce417139c507fb349c2d277998490962b
Craig Zheng <zhhzgreat@hotmail.com>
2015-04-18 05:45:07 +1000

    changed bundle ID, checkout the PHP folder from the march branch

--- civ server side/php/remote_configuration.php
+++ civ server side/php/remote_configuration.php
@@ -4,17 +4,22 @@
 	$file = "remote_configuration.json";
 	$censoredFile = "remote_configuration-censored.json";
 	$debugFile = "remote_configuration-debug.json";
+	//V2 configuration
+	$fileV2 = "remote_configuration_v2.json";
+	$censoredFileV2 = "remote_configuration-censored_v2.json";
+	$debugFile = $fileV2;
+	
 	$version = "1.0";
 	if (isset($_GET["version"])) {
 		$version = $_GET["version"];
 	}
-	$json = json_decode(file_get_contents($file), true);
+	$json = json_decode(file_get_contents($fileV2), true);
 	if ($json) {
 		$json["app_version"] = $version;
 	}
 	
-	if (strcasecmp($version, "2.2") == 0)
-		echo file_get_contents($censoredFile);
+	if (strcasecmp($version, "2.3") == 0)
+		echo file_get_contents($censoredFileV2);
 	else if (strcasecmp($version, "DEBUG") == 0)
 		echo file_get_contents($debugFile);
 	else 
