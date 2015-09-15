bb4824bfef8273b2efd946506d066035847b5131
Craig Zheng <zhhzgreat@hotmail.com>
2015-09-15 20:31:10 +1000

    Added flash to the czzThreadTableViewCommandStatusCellViewController.

--- CustomImageBoardViewer/czzThreadViewCommandStatusCellViewController.m
+++ CustomImageBoardViewer/czzThreadViewCommandStatusCellViewController.m
@@ -11,7 +11,8 @@
 @interface czzThreadViewCommandStatusCellViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *commandStatusLabel;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *loadingActivityIndicator;
-
+@property (weak, nonatomic) IBOutlet UIView *orangeOverlayView;
+@property (assign, nonatomic) BOOL up;
 @end
 
 @implementation czzThreadViewCommandStatusCellViewController
@@ -19,6 +20,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.cellType = czzThreadViewCommandStatusCellViewTypeLoadMore;
+    self.orangeOverlayView.alpha = 0;
+    self.orangeOverlayView.backgroundColor = [settingCentre barTintColour];
     
     [self renderContent];
 }
@@ -32,9 +35,11 @@
             switch (self.cellType) {
                 case czzThreadViewCommandStatusCellViewTypeLoadMore:
                     commandLabelString = @"点击以加载";
+                    self.up = NO;
                     break;
                 case czzThreadViewCommandStatusCellViewTypeReleaseToLoadMore:
                     commandLabelString = @"松开以加载";
+                    self.up = YES;
                     break;
                 case czzThreadViewCommandStatusCellViewTypeNoMore:
                     commandLabelString = @"没有更多内容，点击以加载";
@@ -54,12 +59,28 @@
     }
 }
 
+// Flash the content view, grab user's attention.
+-(void)flash {
+    self.orangeOverlayView.alpha = 0.5;
+    [UIView animateWithDuration:0.2 animations:^{
+        self.orangeOverlayView.alpha = 0;
+    }];
+}
+
 #pragma mark - Setters
 - (void)setCellType:(czzThreadViewCommandStatusCellViewType)cellType {
     if (_cellType != cellType) {
         _cellType = cellType;
     }
     [self renderContent];
+}
+
+// If value changed, flash once.
+- (void)setUp:(BOOL)up {
+    if (_up != up) {
+        [self flash];
+    }
+    _up = up;
 }
 
 + (instancetype)new {
