b6bc36d35c8dbabf3abb13f60c0327028b14ab96
Craig Zheng <zhhzgreat@hotmail.com>
2015-09-20 17:03:42 +1000

    Added czzWKForum class, to be used with czzWKHomeViewController.

---
+++ CustomImageBoardViewer WatchKit Extension/czzWKForum.h
@@ -0,0 +1,13 @@
+//
+//  czzWKForum.h
+//  CustomImageBoardViewer
+//
+//  Created by Craig Zheng on 20/09/2015.
+//  Copyright © 2015 Craig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface czzWKForum : NSObject
+
+@end
---
+++ CustomImageBoardViewer WatchKit Extension/czzWKForum.m
@@ -0,0 +1,13 @@
+//
+//  czzWKForum.m
+//  CustomImageBoardViewer
+//
+//  Created by Craig Zheng on 20/09/2015.
+//  Copyright © 2015 Craig. All rights reserved.
+//
+
+#import "czzWKForum.h"
+
+@implementation czzWKForum
+
+@end
