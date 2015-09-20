e15b71a6099cd1b2d2828322344000020f15d721
Craig Zheng <zhhzgreat@hotmail.com>
2015-09-20 17:46:30 +1000

    Added czzWKForumViewController for the first interface.

---
+++ CustomImageBoardViewer WatchKit Extension/czzWKForumViewController.h
@@ -0,0 +1,16 @@
+//
+//  czzWKForumViewController.h
+//  CustomImageBoardViewer
+//
+//  Created by Craig Zheng on 20/09/2015.
+//  Copyright © 2015 Craig. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+#import "czzWKForum.h"
+
+@interface czzWKForumViewController : WKInterfaceController
+
+@end
---
+++ CustomImageBoardViewer WatchKit Extension/czzWKForumViewController.m
@@ -0,0 +1,70 @@
+//
+//  czzWKForumViewController.m
+//  CustomImageBoardViewer
+//
+//  Created by Craig Zheng on 20/09/2015.
+//  Copyright © 2015 Craig. All rights reserved.
+//
+
+#import "czzWKForumViewController.h"
+
+#import "czzWatchKitCommand.h"
+
+#define wkForumsRowControllerIdentifier @"wkForumsRowControllerIdentifier"
+
+@interface czzWKForumRowController : NSObject
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceLabel *forumNameLabel;
+
+@end
+
+@implementation czzWKForumRowController
+
+
+@end
+
+@interface czzWKForumViewController ()
+@property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceTable *wkForumsTableView;
+@property (strong, nonatomic) NSMutableArray *wkForums;
+@end
+
+@implementation czzWKForumViewController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+-(void)loadForumData {
+    [WKInterfaceController openParentApplication:@{watchKidCommand : @(watchKitCommandLoadForumView)} reply:^(NSDictionary * _Nonnull replyInfo, NSError * _Nullable error) {
+        self.wkForums = [NSMutableArray new];
+        for (NSDictionary *dict in [replyInfo objectForKey:@(watchKitCommandLoadForumView)]) {
+            czzWKForum *forum = [[czzWKForum alloc] initWithDictionary:dict];
+            [self.wkForums addObject:forum];
+        }
+        [self reloadTableView];
+    }];
+}
+
+#pragma mark - TableView
+-(void)reloadTableView {
+    [self.wkForumsTableView setNumberOfRows:self.wkForums.count withRowType:wkForumsRowControllerIdentifier];
+    [self.wkForums enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        czzWKForumRowController *rowController = [self.wkForumsTableView rowControllerAtIndex:idx];
+        [rowController.forumNameLabel setText:[(czzWKForum*)obj name]];
+    }];
+}
+
+@end
+
+
