33c11b9e375f99bcdeea0e5afd07a298bd5042dc
Craig Zheng <zhhzgreat@hotmail.com>
2015-10-02 23:55:06 +1000

    Copied some classes I developed for another project to this project.

---
+++ CustomImageBoardViewer/UIView+Util.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Util.h
+//  CustomImageBoardViewer
+//
+//  Created by Craig Zheng on 2/10/2015.
+//  Copyright © 2015 Craig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Util)
+
+@end
---
+++ CustomImageBoardViewer/UIView+Util.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Util.m
+//  CustomImageBoardViewer
+//
+//  Created by Craig Zheng on 2/10/2015.
+//  Copyright © 2015 Craig. All rights reserved.
+//
+
+#import "UIView+Util.h"
+
+@implementation UIView (Util)
+
+@end
