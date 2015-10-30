43ae0f83068abdef106c41794ccd6201519eec48
Craig Zheng <zhhzgreat@hotmail.com>
2015-10-31 01:08:29 +1100

    Use self sizing cell for thread content table view.

--- CustomImageBoardViewer/czzHomeViewDelegate.m
+++ CustomImageBoardViewer/czzHomeViewDelegate.m
@@ -81,11 +81,11 @@
 
 -(CGFloat)tableView:(UITableView *)tableView estimatedHeightForRowAtIndexPath:(NSIndexPath *)indexPath {
     CGFloat estimatedRowHeight = 44;
-    if (indexPath.row < self.viewModelManager.threads.count) {
-        czzThread *thread = [self.viewModelManager.threads objectAtIndex:indexPath.row];
+    if (indexPath.row < self.homeViewManager.threads.count) {
+        czzThread *thread = [self.homeViewManager.threads objectAtIndex:indexPath.row];
         // If the height is already available.
         NSString *threadID = [NSString stringWithFormat:@"%ld", (long)thread.ID];
-        NSMutableDictionary *heightDictionary = UIInterfaceOrientationIsPortrait([UIApplication sharedApplication].keyWindow.rootViewController.interfaceOrientation) ? self.viewModelManager.verticalHeights : self.viewModelManager.horizontalHeights;
+        NSMutableDictionary *heightDictionary = UIInterfaceOrientationIsPortrait([UIApplication sharedApplication].keyWindow.rootViewController.interfaceOrientation) ? self.homeViewManager.verticalHeights : self.homeViewManager.horizontalHeights;
 
         id cachedHeight = [heightDictionary objectForKey:threadID];
         if ([cachedHeight isKindOfClass:[NSNumber class]]) {
@@ -103,40 +103,40 @@
     return estimatedRowHeight;
 }
 
--(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    if (!self.myTableView) {
-        self.myTableView = (czzThreadTableView*)tableView;
-    }
-    if (indexPath.row >= self.viewModelManager.threads.count)
-        return tableView.rowHeight;
-    
-    CGFloat preferHeight = tableView.rowHeight;
-    czzThread *thread = [self.viewModelManager.threads objectAtIndex:indexPath.row];
-    NSString *threadID = [NSString stringWithFormat:@"%ld", (long)thread.ID];
-    
-    NSMutableDictionary *heightDictionary = UIInterfaceOrientationIsPortrait([UIApplication sharedApplication].keyWindow.rootViewController.interfaceOrientation) ? self.viewModelManager.verticalHeights : self.viewModelManager.horizontalHeights;
-    
-    NSNumber *heightNumber = [heightDictionary objectForKey:threadID];
-    if ([heightNumber isKindOfClass:[NSNumber class]]) {
-        preferHeight = [heightNumber floatValue];
-    } else {
-        preferHeight = [czzTextViewHeightCalculator calculatePerfectHeightForThreadContent:thread inView:self.myTableView hasImage:thread.thImgSrc.length];
-        heightNumber = @(preferHeight);
-        [heightDictionary setObject:heightNumber forKey:threadID];
-    }
-
-//    NSArray *heightArray = UIInterfaceOrientationIsPortrait([UIApplication sharedApplication].keyWindow.rootViewController.interfaceOrientation) ? self.viewModelManager.verticalHeights : self.viewModelManager.horizontalHeights;
-//    CGFloat preferHeight = tableView.rowHeight;
-//    @try {
-//        if (indexPath.row < heightArray.count)
-//            preferHeight = [[heightArray objectAtIndex:indexPath.row] floatValue];
+//-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+//    if (!self.myTableView) {
+//        self.myTableView = (czzThreadTableView*)tableView;
 //    }
-//    @catch (NSException *exception) {
-//        DLog(@"%@", exception);
-//    }
+//    if (indexPath.row >= self.homeViewManager.threads.count)
+//        return tableView.rowHeight;
 //    
-    return preferHeight;
-}
+//    CGFloat preferHeight = tableView.rowHeight;
+//    czzThread *thread = [self.homeViewManager.threads objectAtIndex:indexPath.row];
+//    NSString *threadID = [NSString stringWithFormat:@"%ld", (long)thread.ID];
+//    
+//    NSMutableDictionary *heightDictionary = UIInterfaceOrientationIsPortrait([UIApplication sharedApplication].keyWindow.rootViewController.interfaceOrientation) ? self.homeViewManager.verticalHeights : self.homeViewManager.horizontalHeights;
+//    
+//    NSNumber *heightNumber = [heightDictionary objectForKey:threadID];
+//    if ([heightNumber isKindOfClass:[NSNumber class]]) {
+//        preferHeight = [heightNumber floatValue];
+//    } else {
+//        preferHeight = [czzTextViewHeightCalculator calculatePerfectHeightForThreadContent:thread inView:self.myTableView hasImage:thread.thImgSrc.length];
+//        heightNumber = @(preferHeight);
+//        [heightDictionary setObject:heightNumber forKey:threadID];
+//    }
+//
+////    NSArray *heightArray = UIInterfaceOrientationIsPortrait([UIApplication sharedApplication].keyWindow.rootViewController.interfaceOrientation) ? self.homeViewManager.verticalHeights : self.homeViewManager.horizontalHeights;
+////    CGFloat preferHeight = tableView.rowHeight;
+////    @try {
+////        if (indexPath.row < heightArray.count)
+////            preferHeight = [[heightArray objectAtIndex:indexPath.row] floatValue];
+////    }
+////    @catch (NSException *exception) {
+////        DLog(@"%@", exception);
+////    }
+////    
+//    return preferHeight;
+//}
 
 #pragma mark - UIScrollViewDelegate
 -(void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
