d7c1861028042f5a6db2be70e313c67c605d445a
Craig Zheng <zhhzgreat@hotmail.com>
2015-11-19 23:10:56 +1100

    Removed the old watch os 1 target, copied all resources to watch os 2 target.

---
+++ CIVWatchApp Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  CIVWatchApp Extension
+//
+//  Created by Craig Zheng on 19/11/2015.
+//  Copyright © 2015 Craig. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
