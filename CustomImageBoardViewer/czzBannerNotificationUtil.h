ca2e51ca3d3352c0ab347abb394f7cdfe2ce88d5
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-02-03 15:16:19 +1100

    Added czzBannerNotificationUtil class, this class will be responsible for displaying a banner-like notification in the future. Also put some placeholder methods in this class.

---
+++ CustomImageBoardViewer/czzBannerNotificationUtil.h
@@ -0,0 +1,28 @@
+//
+//  czzBannerNotification.h
+//  CustomImageBoardViewer
+//
+//  Created by Craig on 3/02/2016.
+//  Copyright © 2016 Craig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, BannerNotificationType) {
+    BannerNotificationTypeMessage,
+    BannerNotificationTypeMessageWithResponse
+};
+
+typedef NS_ENUM(NSInteger, BannerNotificationPosition) {
+    BannerNotificationPositionTop,
+    BannerNotificationPositionTopBottom
+};
+
+@interface czzBannerNotificationUtil : NSObject
+
++ (void)displayMessage:(NSString *)message position:(BannerNotificationPosition)position;
++ (void)displayMessage:(NSString *)message
+              position:(BannerNotificationPosition)position
+     completionHandler:(void(^)(void))completionHandler;
+
+@end
---
+++ CustomImageBoardViewer/czzBannerNotificationUtil.m
@@ -0,0 +1,13 @@
+//
+//  czzBannerNotification.m
+//  CustomImageBoardViewer
+//
+//  Created by Craig on 3/02/2016.
+//  Copyright © 2016 Craig. All rights reserved.
+//
+
+#import "czzBannerNotificationUtil.h"
+
+@implementation czzBannerNotificationUtil
+
+@end
