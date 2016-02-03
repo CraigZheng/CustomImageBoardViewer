23d9fb38530d14a75ac657da6a06523f81fdc0d3
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-02-03 21:43:40 +1100

    czzBannerNotificationUtil now properly finds the position and display a banner view, also roughly handles the rotation event.

--- CustomImageBoardViewer/czzBannerNotificationUtil.m
+++ CustomImageBoardViewer/czzBannerNotificationUtil.m
@@ -11,30 +11,89 @@
 #import "czzBannerView.h"
 #import "UIApplication+Util.h"
 
+static NSTimeInterval defaultDisplayTime = 2.0;
+
 @interface czzBannerNotificationUtil() <czzBannerViewDelegate>
 
 @property (nonatomic, strong) czzBannerView *bannerView;
+@property (nonatomic, strong) NSTimer *dismissTimer;
 
 @end
 
 @implementation czzBannerNotificationUtil
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(handleUIApplicationDidChangeStatusBarOrientation)
+                                                     name:UIApplicationDidChangeStatusBarOrientationNotification
+                                                   object:nil];
+    }
+    return self;
+}
+
 - (void)displayMessage:(NSString *)message position:(BannerNotificationPosition)position completionHandler:(void (^)(void))completionHandler {
     DLog(@"message: %@", message);
-    UIViewController *topViewController = [UIApplication topViewController];
     self.bannerView.title = message;
-    if (!topViewController.navigationController.toolbarHidden) {
-        if (self.bannerView.superview) {
-            [self.bannerView removeFromSuperview];
-        }
-        [topViewController.view addSubview:self.bannerView];
-        CGRect targetFrame = topViewController.navigationController.toolbar.frame;
-        targetFrame.origin.y -= self.bannerView.intrinsicContentSize.height;
-        targetFrame.size.height = self.bannerView.intrinsicContentSize.height;
-        self.bannerView.frame = targetFrame;
-    } else {
-        
+    [self displayBannerView: position];
+}
+
+- (void)displayBannerView:(BannerNotificationPosition)position {
+    UIViewController *topViewController = [UIApplication topViewController];
+    if (!topViewController) {
+        return;
     }
+
+    CGRect referenceFrame;
+    if (position == BannerNotificationPositionTop) {
+        if (!topViewController.navigationController.navigationBarHidden) {
+            referenceFrame = topViewController.navigationController.navigationBar.frame;
+        } else {
+            // If hidden, show at the top most position instead.
+            referenceFrame = CGRectMake(0, 0, CGRectGetWidth(topViewController.view.frame), 0);
+        }
+    } else {
+        referenceFrame = topViewController.navigationController.toolbar.frame;
+        if (!topViewController.navigationController.toolbarHidden) {
+            referenceFrame = topViewController.navigationController.toolbar.frame;
+        } else {
+            // No tool bar, show at the bottom of the screen.
+            referenceFrame = CGRectMake(0, CGRectGetHeight(topViewController.view.frame), CGRectGetWidth(topViewController.view.frame), 0);
+        }
+    }
+    if (self.bannerView.superview) {
+        [self.bannerView removeFromSuperview];
+    }
+    if (position == BannerNotificationPositionTop) {
+        // Position is top, move the banner down.
+        referenceFrame.origin.y += self.bannerView.intrinsicContentSize.height;
+    } else {
+        // Position is bottom, move the banner up.
+        referenceFrame.origin.y -= self.bannerView.intrinsicContentSize.height;
+    }
+    referenceFrame.size.height = self.bannerView.intrinsicContentSize.height;
+    self.bannerView.frame = referenceFrame;
+    [topViewController.view addSubview:self.bannerView];
+}
+
+#pragma mark - Rotation event
+- (void)handleUIApplicationDidChangeStatusBarOrientation {
+    // TODO: dismiss first.
+    // TODO: pass the position.
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self displayBannerView:BannerNotificationPositionTop];
+    });
+}
+
+#pragma mark - czzBannerViewDelegate
+
+- (void)bannerViewDidTouch:(czzBannerView *)bannerView {
+    DLog(@"");
+}
+
+- (void)bannerView:(czzBannerView *)bannerView didTouchButton:(UIButton *)button {
+    DLog(@"");
 }
 
 #pragma mark - Getters
