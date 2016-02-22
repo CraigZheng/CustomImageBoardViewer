96beed3e0c3249e9f1933508d71c577b549f047c
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-02-22 13:07:22 +1100

    Added czzPostSenderManagerViewController to its storyboard.

--- CustomImageBoardViewer/czzPostSenderManagerViewController.m
+++ CustomImageBoardViewer/czzPostSenderManagerViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "czzPostSenderManagerViewController.h"
+#import "UIImage+animatedGIF.h"
+#import "czzPostSenderManager.h"
 
-@interface czzPostSenderManagerViewController ()
+@interface czzPostSenderManagerViewController ()<czzPostSenderManagerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *indicatorImageView;
 
 @end
 
@@ -16,22 +19,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [PostSenderManager addDelegate:self];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - czzPostSenderManagerDelegate
+
+- (void)postSenderManager:(czzPostSenderManager *)manager startPostingForSender:(czzPostSender *)postSender {
+    NSURL *gifURL = [[NSBundle mainBundle] URLForResource:@"running_reed"
+                                            withExtension:@"gif"];
+    self.indicatorImageView.image = [UIImage animatedImageWithAnimatedGIFURL:gifURL];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)postSenderManager:(czzPostSenderManager *)manager postingCompletedForSender:(czzPostSender *)postSender success:(BOOL)success message:(NSString *)message {
+    self.indicatorImageView.image = nil;
 }
-*/
 
 @end
