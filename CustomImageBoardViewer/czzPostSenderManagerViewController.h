a2df03a46803fd5b98e48a7597f4f753e286e8fb
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-02-22 12:59:38 +1100

    Added PostSenderManager storyboard and view controller, which will be responsible for displaying data of czzPostSenderManager.

---
+++ CustomImageBoardViewer/czzPostSenderManagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  czzPostSenderManagerViewController.h
+//  CustomImageBoardViewer
+//
+//  Created by Craig on 22/02/2016.
+//  Copyright © 2016 Craig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface czzPostSenderManagerViewController : UIViewController
+
+@end
