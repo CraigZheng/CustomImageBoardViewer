60f8e1943a28a1b32a431aca0a07dbaa13067091
Craig Zheng <craignineten@gmail.com>
2016-05-27 20:11:37 +1000

    Shorten the time of delay for showing warning.

--- CustomImageBoardViewer/czzPostSenderManager.m
+++ CustomImageBoardViewer/czzPostSenderManager.m
@@ -98,7 +98,7 @@
         if (settingCentre.sensitive_keyword.length && [response containsString:settingCentre.sensitive_keyword]) {
             self.severeWarnedPostSender = postSender;
             // Warn all delegates after a set of delay.
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                 [self iterateDelegatesWithBlock:^(id<czzPostSenderManagerDelegate> delegate) {
                     if ([delegate respondsToSelector:@selector(postSenderManager:severeWarningReceivedForPostSender:)]) {
                         [delegate postSenderManager:self
