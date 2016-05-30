9d98eefbe39ee63937b7ef35fe50d96ead31fc1c
Craig Zheng <craignineten@gmail.com>
2016-05-30 22:59:39 +1000

    Added czzURLHandler to allow handling URLs in one place.

---
+++ CustomImageBoardViewer/URL Handler/czzURLHandler.m
@@ -0,0 +1,51 @@
+//
+//  czzURLHandler.m
+//  CustomImageBoardViewer
+//
+//  Created by Craig Zheng on 30/05/2016.
+//  Copyright © 2016 Craig. All rights reserved.
+//
+
+#import "czzURLHandler.h"
+
+#import "czzSettingsCentre.h"
+#import "czzHomeViewManager.h"
+#import "czzAppDelegate.h"
+
+@implementation czzURLHandler
+
+- (BOOL)handleURL:(NSURL *)url {
+    BOOL isHandled = NO;
+    if (url) {
+        NSString *hostPrefix = [settingCentre a_isle_host];
+        if (hostPrefix.length && [url.absoluteString rangeOfString:hostPrefix options:NSCaseInsensitiveSearch].location != NSNotFound) {
+            NSString *threadIDString = [url.absoluteString stringByReplacingOccurrencesOfString:hostPrefix withString:@""];
+            // Text cannot be parsed to an integer, return...
+            threadIDString = [threadIDString componentsSeparatedByString:@"/"].lastObject;
+            NSInteger threadID = threadIDString.integerValue;
+            if (threadID > 0) {
+                isHandled = YES;
+                // Get the thread with the given ID from server.
+                [[czzAppDelegate sharedAppDelegate] showToast:[NSString stringWithFormat:@"正在下载: %ld", (long)threadID]];
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                    czzThread * thread = [[czzThread alloc] initWithThreadID:threadID];
+                    // After return, run the remaining codes in main thread.
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        if (thread) {
+                            [[czzHomeViewManager sharedManager] showContentWithThread:thread];
+                        } else {
+                            [[czzAppDelegate sharedAppDelegate] showToast:[NSString stringWithFormat:@"找不到引用串：%ld", (long)thread.ID]];
+                        }
+                    });
+                });
+            }
+        } else {
+            if ([[UIApplication sharedApplication] canOpenURL:url]) {
+                [[UIApplication sharedApplication] openURL:url];
+                isHandled = YES;
+            }
+        }
+    }
+    return isHandled;
+}
+@end
