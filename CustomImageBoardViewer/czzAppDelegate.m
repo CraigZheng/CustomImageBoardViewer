0a785f78fd79fc194a74b02f6fce454863c51d7a
Craig Zheng <craignineten@gmail.com>
2016-05-30 21:07:22 +1000

    Minor adjustment - allowIDFACollection cannot be found on id object.

--- CustomImageBoardViewer/czzAppDelegate.m
+++ CustomImageBoardViewer/czzAppDelegate.m
@@ -60,9 +60,8 @@
     NSError *configureError;
     [[GGLContext sharedInstance] configureWithError:&configureError];
     NSAssert(!configureError, @"Error configuring Google services: %@", configureError);
-    id tracker = [[GAI sharedInstance] defaultTracker];
     // Enable IDFA collection.
-    tracker.allowIDFACollection = YES;
+    [[[GAI sharedInstance] defaultTracker] setAllowIDFACollection:YES];
     
     //    // Optional: configure GAI options.
     //    GAI *gai = [GAI sharedInstance];
