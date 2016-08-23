377da70d60b04dc8f6ea90380341b02e3c48f0e0
Craig Zheng <craignineten@gmail.com>
2016-08-24 08:22:12 +1000

    Hooked up the table view datasource, tableview delegate and some UI actions for czzAddForumTableViewController.

--- CustomImageBoardViewer/czzAddForumTableViewController.m
+++ CustomImageBoardViewer/czzAddForumTableViewController.m
@@ -8,6 +8,11 @@
 
 #import "czzAddForumTableViewController.h"
 
+#import "czzForum.h"
+#import "czzForumManager.h"
+
+static NSString * cellIdentifier = @"cellIdentifier";
+
 @interface czzAddForumTableViewController ()
 
 @end
@@ -24,10 +29,44 @@
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
+#pragma mark - UITableViewDataSource, UITableViewDelegate
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [czzForumManager sharedManager].customForums.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier
+                                                            forIndexPath:indexPath];
+    czzForum *forum = [czzForumManager sharedManager].customForums[indexPath.row];
+    cell.textLabel.text = forum.name;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%ld", (long)forum.forumID];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
 #pragma mark - UI actions.
 
 - (IBAction)cancelButtonAction:(id)sender {
     [self dismissViewControllerAnimated:true completion:nil];
+}
+
+- (IBAction)editButtonAction:(id)sender {
+    [self.tableView setEditing:!self.tableView.editing animated:YES];
+}
+
+- (IBAction)addButtonAction:(id)sender {
 }
 
 @end
