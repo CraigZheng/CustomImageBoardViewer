e2385c25c931c1d7177f41eaf7c5a76d5cf8ff26
Craig Zheng <craignineten@gmail.com>
2016-08-24 09:21:50 +1000

    Send a forum picked notification upon selecting one of the custom forums.

--- CustomImageBoardViewer/czzCustomForumTableViewManager.m
+++ CustomImageBoardViewer/czzCustomForumTableViewManager.m
@@ -11,6 +11,7 @@
 #import "czzForum.h"
 #import "czzForumManager.h"
 #import "czzSettingsCentre.h"
+#import "czzForumsViewController.h" 
 
 @implementation czzCustomForumTableViewManager
 
@@ -25,6 +26,15 @@
         return 1;
     }
     return [czzForumManager sharedManager].customForums.count;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    czzForum *forum = [[czzForumManager sharedManager].customForums objectAtIndex:indexPath.row];
+    //POST a local notification to inform other view controllers that a new forum is picked
+    NSMutableDictionary *userInfo = [NSMutableDictionary new];
+    [userInfo setObject:forum forKey:kPickedForum];
+    [[NSNotificationCenter defaultCenter] postNotificationName:kForumPickedNotification object:self userInfo:userInfo];
+    [[NavigationManager delegate].viewDeckController closeLeftViewAnimated:YES];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
