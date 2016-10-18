8df10742c3fdd76a378b9088867a796524d23fdf
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-10-18 13:46:31 +1100

    Added czzMarkerManager, which is a class for recording user defined marked UID.

---
+++ CustomImageBoardViewer/czzMarkerManager.h
@@ -0,0 +1,13 @@
+//
+//  czzMarkerManager.h
+//  CustomImageBoardViewer
+//
+//  Created by Craig on 18/10/16.
+//  Copyright © 2016 Craig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface czzMarkerManager : NSObject
+
+@end
---
+++ CustomImageBoardViewer/czzMarkerManager.m
@@ -0,0 +1,13 @@
+//
+//  czzMarkerManager.m
+//  CustomImageBoardViewer
+//
+//  Created by Craig on 18/10/16.
+//  Copyright © 2016 Craig. All rights reserved.
+//
+
+#import "czzMarkerManager.h"
+
+@implementation czzMarkerManager
+
+@end
