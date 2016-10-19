b8365531da7226595c62849b1723f9ac3989c1f3
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-10-19 13:32:25 +1100

    Added the setting of navigation bar colours.

--- CustomImageBoardViewer/AddMarkerViewController.swift
+++ CustomImageBoardViewer/AddMarkerViewController.swift
@@ -16,20 +16,15 @@ class AddMarkerViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Navigation bar colours.
+        if let navigationController = navigationController {
+            navigationController.navigationBar.barTintColor = czzSettingsCentre.sharedInstance().barTintColour()
+            navigationController.navigationBar.tintColor = czzSettingsCentre.sharedInstance().tintColour()
+            navigationController.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName:navigationController.navigationBar.tintColor]
+        }
+        view.backgroundColor = czzSettingsCentre.sharedInstance().viewBackgroundColour()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
