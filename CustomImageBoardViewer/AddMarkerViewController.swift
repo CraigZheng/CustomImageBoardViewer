3d08dffaecde26c2db34736a80e2dba157173a29
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-10-19 13:54:24 +1100

    Made AddMarkerViewController UITableVIewController.

--- CustomImageBoardViewer/AddMarkerViewController.swift
+++ CustomImageBoardViewer/AddMarkerViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AddMarkerViewController: UIViewController {
+class AddMarkerViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
