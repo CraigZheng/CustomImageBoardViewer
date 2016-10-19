88d4a5e5572844082020036db0d6e2caa6b2fddc
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-10-19 13:17:08 +1100

    Added AddMarker storyboard and view controller.

---
+++ CustomImageBoardViewer/AddMarkerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddMarkerViewController.swift
+//  CustomImageBoardViewer
+//
+//  Created by Craig on 19/10/16.
+//  Copyright © 2016 Craig. All rights reserved.
+//
+
+import UIKit
+
+class AddMarkerViewController: NSObject {
+
+}
