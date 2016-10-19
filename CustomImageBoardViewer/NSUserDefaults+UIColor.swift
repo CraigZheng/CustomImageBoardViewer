a333fca3c8d78bf2926e805ebd7beba311dce9d7
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-10-19 13:05:53 +1100

    Added NSUserDefaults+UIColor swift class, and converted it to swift 3 syntax.

---
+++ CustomImageBoardViewer/NSUserDefaults+UIColor.swift
@@ -0,0 +1,29 @@
+//
+//  NSUserDefaults+UIColor.swift
+//  CustomImageBoardViewer
+//
+//  Created by Craig on 19/10/16.
+//  Copyright © 2016 Craig. All rights reserved.
+//
+
+import Foundation
+
+extension UserDefaults {
+    
+    func colorForKey(_ key: String) -> UIColor? {
+        var color: UIColor?
+        if let colorData = data(forKey: key) {
+            color = NSKeyedUnarchiver.unarchiveObject(with: colorData) as? UIColor
+        }
+        return color
+    }
+    
+    func setColor(_ color: UIColor?, forKey key: String) {
+        var colorData: Data?
+        if let color = color {
+            colorData = NSKeyedArchiver.archivedData(withRootObject: color)
+        }
+        set(colorData, forKey: key)
+    }
+    
+}
