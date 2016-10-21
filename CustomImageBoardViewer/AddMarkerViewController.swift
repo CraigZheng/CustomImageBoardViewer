e625a911a83aece794ca2b48c1cb4862d69e4632
Craig Zheng <craignineten@gmail.com>
2016-10-21 18:57:39 +1100

    Use light grey colour flag, rather than the skeleton, for blocked UIDs.

--- CustomImageBoardViewer/AddMarkerViewController.swift
+++ CustomImageBoardViewer/AddMarkerViewController.swift
@@ -124,13 +124,19 @@ extension AddMarkerViewController {
         }
         cell.detailTextLabel?.text = "请选择颜色..."
         cell.contentView.backgroundColor = czzSettingsCentre.sharedInstance().viewBackgroundColour()
+        cell.backgroundColor = cell.contentView.backgroundColor
         if let UID = UID {
             cell.textLabel?.text = UID
-            if let cell = cell as? UIDColourPairCellTableViewCell,
-                let colour = czzMarkerManager.sharedInstance().highlightColour(forUID: UID) {
-                // Assign an image as the template for cell.imageView.
-                cell.imageView?.image = UIImage.init(named: "flag")?.withRenderingMode(.alwaysTemplate)
-                cell.imageView?.tintColor = colour
+            if let cell = cell as? UIDColourPairCellTableViewCell {
+                if czzMarkerManager.sharedInstance().isUIDBlocked(UID) {
+                    // Assign the skeleton image.
+                    cell.imageView?.image = UIImage.init(named: "flag")?.withRenderingMode(.alwaysTemplate)
+                    cell.imageView?.tintColor = UIColor.lightGray
+                } else if let colour = czzMarkerManager.sharedInstance().highlightColour(forUID: UID) {
+                    // Assign an image as the template for cell.imageView.
+                    cell.imageView?.image = UIImage.init(named: "flag")?.withRenderingMode(.alwaysTemplate)
+                    cell.imageView?.tintColor = colour
+                }
             }
         }
         return cell
