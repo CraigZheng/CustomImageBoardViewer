753d9ccb1655d24722eb3437505e4dc20b1adf71
Haozheng Zheng <haozheng.zheng@optus.com.au>
2016-10-21 16:24:49 +1100

    MarkerColourPickerViewController now also can be used as blocking.

--- CustomImageBoardViewer/MarkerColourPickerViewController.swift
+++ CustomImageBoardViewer/MarkerColourPickerViewController.swift
@@ -28,6 +28,7 @@ class MarkerColourPickerViewController: UIViewController {
     
     // RRGGBB hex colors in the same order as the image
     private let colorArray = [ 0x000000, 0xfe0000, 0xff7900, 0xffb900, 0xffde00, 0xfcff00, 0xd2ff00, 0x05c000, 0x00c0a7, 0x0600ff, 0x6700bf, 0x9500c0, 0xbf0199, 0xffffff ]
+    private var selectedIndex: Int?
     
     @IBOutlet private weak var uidLabel: UILabel?
     @IBOutlet private weak var flagImageView: UIImageView? {
@@ -37,16 +38,27 @@ class MarkerColourPickerViewController: UIViewController {
     }
     @IBOutlet private weak var slider: UISlider!
     @IBAction func colourSliderValueChanged(_ sender: UISlider) {
-        selectedColour = uiColorFromHex(rgbValue: colorArray[Int(sender.value)])
+        selectedIndex = Int(sender.value)
+        if let selectedIndex = selectedIndex {
+            selectedColour = uiColorFromHex(rgbValue: colorArray[selectedIndex])
+        }
     }
     
     @IBAction func tapOnBackgroundView(_ sender: AnyObject) {
         if let UID = self.UID,
             let selectedColour = self.selectedColour {
-            czzMarkerManager.sharedInstance().highlightUID(UID, withColour: selectedColour)
+            if let selectedIndex = selectedIndex,
+                selectedIndex == colorArray.endIndex
+            {
+                czzMarkerManager.sharedInstance().pendingHighlightUIDs.remove(UID)
+                czzMarkerManager.sharedInstance().blockUID(UID)
+            } else {
+                czzMarkerManager.sharedInstance().unBlockUID(UID)
+                czzMarkerManager.sharedInstance().highlightUID(UID, withColour: selectedColour)
+            }
         }
         _ = navigationController?.popViewController(animated: true)
-        dismiss(animated: true, completion: nil)
+        dismiss(animated: true, completion:nil)
     }
     
     override func viewDidLoad() {
