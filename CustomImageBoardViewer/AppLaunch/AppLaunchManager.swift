df0fa8007b8f8a85e5cc36a91926b3d7b5d98fa9
Craig Zheng <craignineten@gmail.com>
2017-03-12 22:16:46 +1100

    Moved the logic of showing the launch popup to AppLaunchManager.

--- CustomImageBoardViewer/AppLaunch/AppLaunchManager.swift
+++ CustomImageBoardViewer/AppLaunch/AppLaunchManager.swift
@@ -10,4 +10,31 @@ import Foundation
 
 class AppLaunchManager: NSObject {
     static var shared = AppLaunchManager()
+    
+    fileprivate var isRemoteSettingsUpdated = false {
+        didSet {
+            if isRemoteSettingsUpdated && !czzSettingsCentre.sharedInstance().popup_notification_link.isEmpty,
+                let notificationURL = URL(string: czzSettingsCentre.sharedInstance().popup_notification_link) {
+                NSURLConnection.sendAsynchronousRequest(URLRequest(url: notificationURL), queue: .main, completionHandler: { (response, data, error) in
+                    if (response as? HTTPURLResponse)?.statusCode == 200,
+                        let data = data,
+                        let jsonString = String(data: data, encoding: .utf8),
+                        let popupNotification = czzLaunchPopUpNotification(json: jsonString)
+                    {
+                        popupNotification.tryShow()
+                    }
+                })
+            }
+        }
+    }
+    
+    override init() {
+        super.init()
+        NotificationCenter.default.addObserver(forName: .UIApplicationDidBecomeActive, object: nil, queue: OperationQueue.main) { _ in
+            
+        }
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.remoteSettingUpdated, object: nil, queue: OperationQueue.main, using: { [weak self] _ in
+            self?.isRemoteSettingsUpdated = true
+        })
+    }
 }
--- CustomImageBoardViewer/CustomImageBoardViewer-Bridging-Header.h
+++ CustomImageBoardViewer/CustomImageBoardViewer-Bridging-Header.h
@@ -1,6 +1,6 @@
-#import <UIKit/UIKit.h>
 #import "czzAppDelegate.h"
 #import "czzSettingsCentre.h"
 #import "czzMarkerManager.h"
 #import "czzCacheCleaner.h"
 #import "czzSelectionSelectorViewController.h"
+#import "czzLaunchPopUpNotification.h"
