3b42a8424c508920e1ef4a9f86540d0bcf9bf7d4
Craig Zheng <craignineten@gmail.com>
2017-05-13 18:46:16 +1000

    Added another cell to the DraftSelectorTableViewController, tapping on this cell would call DraftManager.clear()

--- CustomImageBoardViewer/DraftSelectorTableViewController.swift
+++ CustomImageBoardViewer/DraftSelectorTableViewController.swift
@@ -14,14 +14,21 @@ import UIKit
 
 class DraftSelectorTableViewController: UITableViewController {
     @objc var delegate: DraftSelectorTableViewControllerDelegate?
+    
     private var drafts: [(String, Date)] = DraftManager.drafts.reversed()
     private let dateFormatter: DateFormatter = {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "hh:mm ddMMM"
         return dateFormatter
     }()
+    
     private struct CellIdentifier {
         static let draft = "draftCell"
+        static let clear = "clearCell"
+    }
+    private enum Section: Int {
+        case drafts = 0
+        case clear
     }
     
     override func viewDidLoad() {
@@ -29,21 +36,46 @@ class DraftSelectorTableViewController: UITableViewController {
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 44
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return drafts.count
+        switch Section(rawValue: section)! {
+        case .drafts: return drafts.count
+        case .clear: return drafts.count > 0 ? 1 : 0
+        }
     }
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch Section(rawValue: section)! {
+        case .drafts: return "草稿"
+        case .clear: return " "
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.draft, for: indexPath)
-        cell.textLabel?.text = drafts[indexPath.row].0
-        cell.detailTextLabel?.text = dateFormatter.string(from: drafts[indexPath.row].1)
-        return cell
+        switch Section(rawValue: indexPath.section)! {
+        case .drafts:
+            let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.draft, for: indexPath)
+            cell.textLabel?.text = drafts[indexPath.row].0
+            cell.detailTextLabel?.text = dateFormatter.string(from: drafts[indexPath.row].1)
+            return cell
+        case .clear: return tableView.dequeueReusableCell(withIdentifier: CellIdentifier.clear, for: indexPath)
+        }
     }
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        delegate?.draftSelector(self, selectedContent: drafts[indexPath.row].0)
+        switch Section(rawValue: indexPath.section)! {
+        case .drafts:
+            delegate?.draftSelector(self, selectedContent: drafts[indexPath.row].0)
+        case .clear:
+            drafts = []
+            DraftManager.clear()
+            tableView.reloadData()
+        }
     }
 }
