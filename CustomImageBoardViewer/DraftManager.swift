ac41dfe497d4e9a6236ea4a998caa2bf4c4d1d11
Craig Zheng <craignineten@gmail.com>
2017-05-13 15:19:45 +1000

    Added DraftManager.swift, a manager class for saving and retrieving draft contents.

---
+++ CustomImageBoardViewer/DraftManager.swift
@@ -0,0 +1,29 @@
+//
+//  DraftManager.swift
+//  CustomImageBoardViewer
+//
+//  Created by Craig Zheng on 13/5/17.
+//  Copyright © 2017 Craig. All rights reserved.
+//
+
+import UIKit
+
+class DraftManager: NSObject {
+
+    var drafts: [String] {
+        return []
+    }
+    
+    func save(_ draft: String) {
+        
+    }
+    
+    func save() {
+        
+    }
+    
+    func clear() {
+        
+    }
+    
+}
