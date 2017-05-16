620e83127a8345b591ee37515a68c964a00fb2da
Craig Zheng <craignineten@gmail.com>
2017-05-17 09:09:44 +1000

    Added a new function to String+CIV, which returns all occurrences of the same string.

--- CustomImageBoardViewer/String+CIV.swift
+++ CustomImageBoardViewer/String+CIV.swift
@@ -15,4 +15,38 @@ extension NSString {
         return components.joined(separator: "")
     }
     
+    @objc func rangesOf(_ searchString: String) -> [NSRange] {
+        var searchRange = NSMakeRange(0, length)
+        var foundRange: NSRange
+        var foundRanges: [NSRange] = []
+        while searchRange.location < length {
+            searchRange.length = length - searchRange.location
+            foundRange = self.range(of: searchString, options: .caseInsensitive, range: searchRange)
+            if foundRange.location != NSNotFound {
+                searchRange.location = foundRange.location + foundRange.length
+                foundRanges.append(foundRange)
+            } else {
+                searchRange.location = NSIntegerMax
+            }
+        }
+        return foundRanges
+    }
+    
+    /*
+     // Copied from http://stackoverflow.com/questions/7033574/find-all-locations-of-substring-in-nsstring-not-just-first
+     NSRange searchRange = NSMakeRange(0,string.length);
+     NSRange foundRange;
+     while (searchRange.location < string.length) {
+     searchRange.length = string.length-searchRange.location;
+     foundRange = [string rangeOfString:substring options:nil range:searchRange];
+     if (foundRange.location != NSNotFound) {
+     // found an occurrence of the substring! do stuff here
+     searchRange.location = foundRange.location+foundRange.length;
+     } else {
+     // no more substring to find
+     break;
+     }
+     }
+ */
+    
 }
