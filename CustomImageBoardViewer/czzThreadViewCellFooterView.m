21c1167855d35ff4d5660c58bad31e6f1376294b
Craig Zheng <craignineten@gmail.com>
2017-07-17 13:53:48 +1000

    Update the footer view hidden priority a bit.

--- CustomImageBoardViewer/czzThreadViewCellFooterView.m
+++ CustomImageBoardViewer/czzThreadViewCellFooterView.m
@@ -34,7 +34,7 @@
 
 -(void)setThread:(czzThread *)myThread {
     _thread = myThread;
-    self.chatImageView.hidden = self.sageLabel.hidden = self.lockedLabel.hidden = self.responseCountLabel.hidden = YES;
+    self.chatImageView.hidden = self.sageLabel.hidden = self.lockedLabel.hidden = self.responseCountLabel.hidden = self.forumLabel.hidden = YES;
     if (myThread) {
         self.sageLabel.hidden = !myThread.sage;
         self.lockedLabel.hidden = !myThread.lock;
@@ -46,15 +46,17 @@
             [[czzForumManager sharedManager].forums enumerateObjectsUsingBlock:^(czzForum * _Nonnull forum, NSUInteger idx, BOOL * _Nonnull stop) {
                 if (myThread.fid == forum.forumID) {
                     self.forumLabel.text = forum.name;
+                    self.forumLabel.hidden = NO;
                     *stop = YES;
                 }
             }];
         } else {
             self.forumLabel.text = nil;
+            self.forumLabel.hidden = YES;
         }
     }
     // If all elements are hidden, shrink the size of this view.
-    if (self.sageLabel.hidden && self.lockedLabel.hidden && self.responseCountLabel.hidden) {
+    if (self.sageLabel.hidden && self.lockedLabel.hidden && self.responseCountLabel.hidden && self.forumLabel.hidden) {
         self.bottomSeparatorVerticalSpacingConstraint.priority = 999;
     } else {
         self.bottomSeparatorVerticalSpacingConstraint.priority = 1;
