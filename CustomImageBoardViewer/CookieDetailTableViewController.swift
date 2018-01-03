753e13ab80cbae18e98476e79eb0e07c89931b89
CraigZheng <zhhzgreat@hotmail.com>
2018-01-03 12:43:20 +1100

    Added CookieDetailTableViewController to the project.

---
+++ CustomImageBoardViewer/CookieDetailTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CookieDetailTableViewController.swift
+//  CustomImageBoardViewer
+//
+//  Created by Haozheng Zheng on 3/1/18.
+//  Copyright © 2018 Craig. All rights reserved.
+//
+
+import UIKit
+
+class CookieDetailTableViewController: UITableViewController {
+    @IBOutlet weak var addButton: RoundCornerBorderedButton!
+    @IBOutlet weak var hostPickerView: UIPickerView!
+    
+    
+}
+
+// MARK: - UIPickerViewDataSource, UIDocumentPickerDelegate
+extension CookieDetailTableViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return ["主岛", "备胎岛"][row]
+    }
+}
