61a97bf6f4cbb28edc180ca38cbfd93eb35dea62
Craig Zheng <craignineten@gmail.com>
2018-01-08 20:11:38 +1100

    Performed ctrl + a, ctrl + i on these view controllers with recommended indentation setting.

--- CustomImageBoardViewer/CookieDetailTableViewController.swift
+++ CustomImageBoardViewer/CookieDetailTableViewController.swift
@@ -9,23 +9,23 @@
 import UIKit
 
 class CookieDetailTableViewController: UITableViewController {
-    @IBOutlet weak var addButton: RoundCornerBorderedButton!
-    @IBOutlet weak var hostPickerView: UIPickerView!
-    
-    
+  @IBOutlet weak var addButton: RoundCornerBorderedButton!
+  @IBOutlet weak var hostPickerView: UIPickerView!
+  
+  
 }
 
 // MARK: - UIPickerViewDataSource, UIDocumentPickerDelegate
 extension CookieDetailTableViewController: UIPickerViewDataSource, UIPickerViewDelegate {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 2
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return ["主岛", "备胎岛"][row]
-    }
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return 2
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return ["主岛", "备胎岛"][row]
+  }
 }
