a8109f85cb00b86f7245aceaba4f507f6454e52d
Craig Zheng <craignineten@gmail.com>
2018-02-05 01:25:15 +1100

    Pass the cookie value to CookieTextInputViewController.

--- CustomImageBoardViewer/CookieDetailTableViewController.swift
+++ CustomImageBoardViewer/CookieDetailTableViewController.swift
@@ -11,12 +11,28 @@ import UIKit
 class CookieDetailTableViewController: UITableViewController {
   @IBOutlet weak var addButton: RoundCornerBorderedButton!
   @IBOutlet weak var hostPickerView: UIPickerView!
+  @IBOutlet weak var cookieValueLabel: UILabel!
+  
   var cookieValue: String?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    if let cookieValue = cookieValue {
+      cookieValueLabel.text = cookieValue
+    }
+  }
   
   @IBAction func resetAction(_ sender: Any) {
   }
   
   @IBAction func addAction(_ sender: Any) {
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let textInputViewController = segue.destination as? CookieTextInputViewController {
+      textInputViewController.prefilledString = cookieValue
+      textInputViewController.delegate = self
+    }
   }
 }
 
@@ -32,5 +48,12 @@ extension CookieDetailTableViewController: UIPickerViewDataSource, UIPickerViewD
   
   func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
     return ["主岛", "备胎岛"][row]
+  }
+}
+
+extension CookieDetailTableViewController: CookieTextInputViewControllerDelegate {
+  func textDetailEntered(_ inputViewController: CookieTextInputViewController, enteredDetails: String) {
+    cookieValue = enteredDetails
+    cookieValueLabel.text = cookieValue
   }
 }
