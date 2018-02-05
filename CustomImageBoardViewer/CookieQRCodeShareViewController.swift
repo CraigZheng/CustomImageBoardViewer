375d85ae4404f6b63515d300b51a99c165d1b535
Craig Zheng <craignineten@gmail.com>
2018-02-05 18:10:45 +1100

    Added CookieQRCodeShareViewController, this view controller would be responsible for displaying a QR code on screen.

---
+++ CustomImageBoardViewer/CookieQRCodeShareViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CookieQRCodeShareViewController.swift
+//  CustomImageBoardViewer
+//
+//  Created by Craig Zheng on 5/2/18.
+//  Copyright © 2018 Craig. All rights reserved.
+//
+
+import UIKit
+
+class CookieQRCodeShareViewController: UIViewController {
+  @IBOutlet weak var qrCodeImageView: UIImageView!
+  
+  
+}
